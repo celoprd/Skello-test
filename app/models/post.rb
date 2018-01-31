987bd87ac0877f82b7a7ec3e21edcb1d2bd77b93
celestin prudhomme <celestin.prudhomme@gmail.com>
2018-01-31 15:22:35 +0100

    form required

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :comments
+  validates :title,  presence: true
+  validates :content,  presence: true
   scope :ordered_by_ratings, -> { order(rating: :desc) }
 
   def vote!
